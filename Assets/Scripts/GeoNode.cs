4ba34a6f56091072d19a0648639d33807b365e61
Alkrin <alkrin@gmail.com>
2021-03-07 22:21:33 -0800

    First pass GeoMaterial, GeoNode.  Stub GeoGrid.

---
+++ Assets/Scripts/GeoNode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoNode : MonoBehaviour {
+    private GeoMaterial geoMaterial;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void Initialize(GeoMaterialName mName) {
+        geoMaterial = gameObject.GetComponentInChildren<GeoMaterial>();
+
+        geoMaterial.Initialize(mName);
+    }
+}
